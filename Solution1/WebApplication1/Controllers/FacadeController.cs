3f85eb90994e937f75adf9bbc008978e8fa3ce4b
jonathan824 <jonathan.m.j.824@gmail.com>
2023-03-17 09:39:47 -0500

    Se crea clase de la fachada

---
+++ Solution1/WebApplication1/Controllers/FacadeController.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class FacadeController
+    {
+        [HttpGet(Name = "ConsultarAutenticacion")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
