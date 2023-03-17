1fa42e3173da0d57b2f7d3794cbd84fb0c97c23b
Jonathan824 <63627462+Jonathan824@users.noreply.github.com>
2023-03-17 10:33:59 -0500

    Se agrega Dal de zapatos

---
+++ Solution1/WebApplication1/Dal/ZapatoDal.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication1.Dal
+{
+    public class ZapatoDal
+    {
+       public Zapato Create(Zapato zapato)
+        {
+            return zapato;
+        }
+
+        public Zapato Read(int id)
+        {
+            return new Zapato();
+        }
+
+        public Zapato Update(Zapato zapato, int id)
+        {
+            return zapato;
+        }
+
+        public Zapato Delete(int id)
+        {
+            return id;
+        }
+    }
+}
