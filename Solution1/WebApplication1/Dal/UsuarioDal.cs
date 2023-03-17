a8d9d04c5d4bfe16ccd429dee1d1461dfe65b929
= <=>
2023-03-17 10:42:28 -0500

    Se crean metodos

--- Solution1/WebApplication1/Dal/UsuarioDal.cs
+++ Solution1/WebApplication1/Dal/UsuarioDal.cs
@@ -2,5 +2,24 @@
 {
     public class UsuarioDal
     {
+        public Usuario Create(Usuario usuario)
+        {
+            return usuario;
+        }
+
+        public Usuario Read(int id)
+        {
+            return new Usuario();
+        }
+
+        public Usuario Update(Usuario usuario, int id)
+        {
+            return usuario;
+        }
+
+        public int Delete(int id)
+        {
+            return id;
+        }
     }
 }
